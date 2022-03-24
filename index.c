54e4ca4039b950340299946a48865ae30de64e6b
Geovanna Nascimento <geogeovannarn@gmail.com>
2022-03-24 12:20:49 +0000

    feat: praticando com exercícios.

--- index.c
+++ index.c
@@ -1,6 +1,83 @@
 #include <stdio.h>
 
 int main(void){
-    printf("oi\n");
+    /* 
+    EXERCÍCIO 1
+
+    float n1, n2, n3, n4, menor, soma;
+    printf("Informe quatro números:\n");
+    scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
+    menor = n1;
+
+    if(n2<menor){
+        menor = n2;
+    }
+    if(n3<menor){
+        menor = n3;
+    }
+    if(n4<menor){
+        menor = n4;
+    }
+    printf("O menor número é o %.2f\n", menor);
+    soma = n1 + n2 + n3 + n4 - menor;
+    printf("E a soma dos três nº maiores é: %.2f\n", soma);
+
+    -------
+
+    EXERCÍCIO 2
+
+    float n1, n2, n3, n4, maior, soma;
+    printf("Informe quatro números: \n");
+    scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
+    maior = n1;
+
+    if(n2 > maior){
+        maior = n2;
+    }
+    if(n3 > maior){
+        maior = n3;
+    }
+    if(n4 > maior){
+        maior = n4;
+    }
+    printf("O maior número é o %.2f\n", maior);
+    soma = n1 + n2 + n3 + n4 - maior;
+    printf("E a soma dos três menores é: %.2f", soma);
+
+    -----
+
+    EXERCÍCIO 3
+
+    float a, b, c, delta;
+    printf("Informe três números: \n");
+    scanf("%f %f %f", &a, &b, &c);
+    delta = (b*b) - (4*a*c);
+
+    printf("Delta: %.2f\n", delta);
+
+    if(delta > 0){
+        printf("Existem duas raízes reais\n");
+    }
+    if(delta < 0){
+        printf("Não existem raízes reais\n");
+    }
+    if(delta == 0){
+        printf("Existe uma raíz real\n");
+    } 
+    
+    ----
+
+    EXERCÍCIO 4
+
+    int num;
+    printf("Informe um número: \n");
+    scanf("%d", &num);
+
+    if(num % 7 == 0){
+        printf("É múltiplo!\n");
+    }
+    if(num % 7 != 0){
+        printf("Não é múltiplo!\n");
+    } */
     return 0;
 }
